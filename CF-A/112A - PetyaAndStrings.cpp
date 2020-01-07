baa2ece819bb7f9f44674aca4e8e372b9898b043
Giovana <giovana.daniele@usp.br>
2020-01-06 21:07:11 -0300

    Adding problems

---
+++ CF-A/112A - PetyaAndStrings.cpp
@@ -0,0 +1,25 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main(){
+    	string s1, s2;
+    	cin >> s1 >> s2;
+     
+    	for(unsigned int i = 0; i < s1.size(); i++){
+    		if(s1[i] < 97) s1[i] += 32;
+    		if(s2[i] < 97) s2[i] += 32;
+     
+    		if(s1[i] < s2[i]){
+    			cout << -1 << endl;
+    			return 0;
+    		}
+    		else if(s2[i] < s1[i]){
+    			cout << 1 << endl;
+    			return 0;
+    		}
+    	}
+     
+    	cout << 0 << endl;
+     
+    	return 0;
+    }
