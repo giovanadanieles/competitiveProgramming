ab4ed28c4261162e01db17c0a8780eec51bf0749
Giovana <giovana.daniele@usp.br>
2020-01-08 20:21:17 -0300

    Adding problem with Greatest Common Denominator (GCD)

---
+++ CF-A/9A - DieRoll.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*int main(){
+	int y, w, solve; 
+
+	cin >> y >> w;
+
+	solve = 6 - max(y, w) + 1;
+
+	if(solve % 6 == 0) cout << "1/1" << endl;
+	else if(solve % 3 == 0) cout << solve / 3 << "/2" << endl;
+	else if(solve % 2 == 0) cout << solve / 2 << "/3" << endl;
+	else cout << solve << "/6" << endl;
+
+	return 0;
+}*/
+
+//Version with Greatest Common Denominator (GCD):
+
+int gcd(int a, int b){
+	return b == 0 ? a : gcd(b, a % b); 
+}
+
+int main(){
+	int y, w, solve;
+
+	cin >> y >> w;
+
+	solve = 6 - max(y, w) + 1;
+
+	cout << solve / gcd(solve, 6) << "/" << 6 / gcd(solve, 6) << endl;
+
+	return 0;
+}
