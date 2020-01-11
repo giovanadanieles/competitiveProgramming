99124f44e542bf4fef6db4e0f62609c4564ddb7e
Giovana <giovana.daniele@usp.br>
2020-01-11 00:23:16 -0300

    Adding problems

---
+++ CF-A/339A - HelpfulMaths.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s; cin >> s;
+	vector <int> v;
+
+	for(unsigned int i = 0; i < s.size(); i+=2){
+		v.push_back(s[i] - '0');
+	}
+
+	sort(v.begin(), v.end());
+
+	for(unsigned int i = 0; i < v.size(); i++){
+		if(i == v.size()-1) cout << v[i] << endl;
+		else cout << v[i] << "+";
+	}
+
+
+
+	return 0;
+}
