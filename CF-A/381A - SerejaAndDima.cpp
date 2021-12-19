fb2b49a371b5555a008edd3c9660a606d64f05ac
Giovana Daniele <giovana.daniele@usp.br>
2021-12-19 13:22:35 -0300

    update solution

--- CF-A/381A - SerejaAndDima.cpp
+++ CF-A/381A - SerejaAndDima.cpp
@@ -46,3 +46,37 @@ int main(){
 
 	return 0;
 }
+
+/*------------------------------------------------------------------------*/
+// two years later:
+
+
+// Memory complexity: O(1)
+//   Time complexity: O(N)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* const argv[]){
+	int nCards, aux, serejaP = 0, dimaP = 0;
+	vector<int> cards;
+
+	cin >> nCards;
+
+	for(int i = 0; i < nCards; i++){
+		cin >> aux;
+		cards.push_back(aux);
+	}
+
+	aux = 1;
+	for(int left = 0, right = nCards - 1; left <= right; aux *= -1){
+		aux == 1 ? serejaP += max(cards[left], cards[right]) : 
+				     dimaP += max(cards[left], cards[right]);
+
+		cards[left] > cards[right] ? left++ : right--;
+	}
+
+	cout << serejaP << " " << dimaP << endl;
+
+	return 0;
+}
