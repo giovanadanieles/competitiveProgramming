00cfd06f2367f80729259821fd4f0e35b451c153
Giovana Daniele <giovana.daniele@usp.br>
2022-01-09 11:14:21 -0300

    new solved problem

---
+++ CF-A/479A - Expression.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* const argv[]){
+	int a, b, c, ans;
+	cin >> a >> b >> c;
+
+	ans = a + b + c;
+
+	ans = max(ans, (a + b) * c);
+	ans = max(ans, a * (b + c));
+	ans = max(ans, a * b * c);
+
+	cout << ans << endl;
+
+	return 0;
+}
